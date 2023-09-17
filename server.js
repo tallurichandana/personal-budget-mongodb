b9904d992c273f4dc49b454cb97e292e562a321b
Chandana Talluri <ctallur1@uncc.edu>
2023-09-17 15:49:39 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -3,23 +3,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-]
-};
+app.use('/bug',express.static('budg-data.json'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello, World!');
