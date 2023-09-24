447fcce5a19a1b1fb54ad66a0cd35b2f8d5cd9ce
Chandana Talluri <ctallur1@uncc.edu>
2023-09-24 18:09:35 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
+// Budget API
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = 3000;
 const fs = require('fs');
 
-app.use(express.static('public'));
+app.use(cors()); // Use cors() as a middleware function.
 
-app.get('/hello', (req, res) => {
-    res.send('Hello, World!');
-});
+// app.use('/', express.static('public'));
+app.use('/bug', express.static('budg-data.json'));
+
+// app.get('/hello', (req, res) => {
+//     res.send('Hello World!');
+// });
 
 app.get('/budget', (req, res) => {
     fs.readFile('budg-data.json', 'utf8', (err, data) => {
         if (err) {
-            console.error('Error reading JSON file:', err);
-            res.status(500).json({ error: 'Internal Server Error' });
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred while reading budget data.' });
         } else {
-            const budget = JSON.parse(data);
-            res.json(budget);
+            const budgetData = JSON.parse(data);
+            res.json(budgetData);
         }
     });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
